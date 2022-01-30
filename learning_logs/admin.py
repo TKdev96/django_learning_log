9a7aa7575ef3811f825c9c0de734ca19c8b6f73d
Tomasz Kaczmarczyk <74932362+TKdev96@users.noreply.github.com>
2022-01-30 12:53:46 +0100

    Aktywacja panelu administracyjnego, superuser, rejestracja modelu, test dodania tematu
    1. Aktywacja Witryny administracyjnej Django oraz Konta superusera - python manage.py createsuperuser
    
    2. Rejestracja modelu Topic w witrynie administracyjnej
    w /learning_logs/admin.py
    - import modelu Topic z learning_logs/models.py
    - rejestracja modelu admin.site.register(Topic)
    
    3. Dodanie testowego Topic przez witrynę administracyjną

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from learning_logs.models import Topic #import modelu Topic z learning_logs.models
 # Register your models here.
+
+admin.site.register(Topic) #Rejestracja obsługi modelu Topic
+
