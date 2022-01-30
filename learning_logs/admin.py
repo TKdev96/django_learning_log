bd597db64df86acc48cde2b0e53ca229c0efbcb9
Tomasz Kaczmarczyk <74932362+TKdev96@users.noreply.github.com>
2022-01-30 13:28:48 +0100

    Utworzenie modelu Entry, migracja, rejestracja w witrynie administracyjnej
    1. Utworzenie modelu Entry:
    Atrybuty modelu:
    - topic - odwołanie do Modelu topic (umożliwia powiązanie wpisów z odpowiednim tematem)
    - text - treść wpisu
    - date_added - data utworzenia wpisu
    funkcja str zwraca string modelu Entry max do 50 znaków, następnie dodając trzykropek
    
    2. Zrobiono migrację modelu Entry:
    - python manage.py makemigrations learning_logs
    - python manage.py migrate
    
    3. Dodano model Entry do obsługi w witrynie administracyjnej
    /learning_logs/admin.py
    zaimportowano model Entry z learning_logs/models.
    i dodano rejestracje admin.site.register(Entry)

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from learning_logs.models import Topic #import modelu Topic z learning_logs.models
+from learning_logs.models import Topic, Entry #import modelu Topic z learning_logs.models later import Entry
 # Register your models here.
 
 admin.site.register(Topic) #Rejestracja obsługi modelu Topic
+admin.site.register(Entry) #Rejestracja obsługi modelu Entry
+
 
--- learning_logs/models.py
+++ learning_logs/models.py
@@ -8,5 +8,19 @@ class Topic(models.Model):
     date_added = models.DateTimeField(auto_now_add=True) #pole data określone po aktualnej dacie i godzinie dodania
 
     def __str__(self):
-        """Zwraca textową reprezentację modelu"""
+        """Zwraca tekstową reprezentację modelu"""
         return self.text
+
+class Entry(models.Model):
+    """Entry to the topic"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE) #ForeignKey pozwala przypisywać entry do Topic, page 508
+    text = models.TextField() #pole tekstowe dla treści wpisu
+    date_added = models.DateTimeField(auto_now_add=True) #data utworzenia wpisu
+
+    class Meta: #Meta przechowuje informacje dodatkowe dla modelu Entry, użycie formy entries podczas odwoływania się do więcej niż tylko jednego wpisu.
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Zwraca tekstowa reprezentację modelu"""
+        return  self.text[:50] + "..." #jeżeli string ma 50 znaków to go ucina i dodaje trzy kropki
+
