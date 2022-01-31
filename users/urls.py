3378e81adf7ea6640926ffe9d7ccdbac16ae434f
Tomasz Kaczmarczyk <74932362+TKdev96@users.noreply.github.com>
2022-01-31 21:24:25 +0100

    Dodanie możliwości rejestracji użytkowników
    1. Dodanie ścieżki register w users/urls.py
    
    2.  Utworzenie widoku register w views.py
    
    3. Utworzenie szablonu register.html
    
    4. Dodanie odnośnika register.html do base.html

--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,5 @@ app_name = 'users' #przypisanie nazwy aplikacji konieczne
 urlpatterns = [
     path('login/', LoginView.as_view(template_name = 'users/login.html'), name='login'), #utworzenie ścieżki z użyciem klasy LoginView i przypisanie własnego szablonu html
     path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register, name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render, redirect #import redirect
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login #import logout, login
+from django.contrib.auth.forms import UserCreationForm #import wbudowanego formularza rejestracyjnego
 
 # Create your views here.
 
-#Utworzenie funkcji wylogowania
+#Utworzenie widoku wylogowania
+
 def logout_view(request):
     logout(request)
     return redirect('learning_logs:index')
+
+#Utworzenie widoku rejestracji użytkownika
+
+def register(request):
+    if request.method != 'POST':
+        form = UserCreationForm()
+    else:
+        form = UserCreationForm(data=request.POST) #przekazanie danych formularza do klasy formularza tworzenia użytkownika
+
+        if form.is_valid():
+            new_user = form.save() #zapisanie w bazie nazwy użytkownika i hasła(hash)
+            login(request, new_user) #autologowanie utworzonego użytkownika
+            return redirect('learning_logs:index') #przekierowanie użytkownika na stronę główną
+
+    context = {'form': form}
+    return render(request, 'users/register.html', context)        
+
