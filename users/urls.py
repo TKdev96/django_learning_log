c86072010cc1ad56ab7a828a54403f7bc4fcda08
Tomasz Kaczmarczyk <74932362+TKdev96@users.noreply.github.com>
2022-01-31 20:51:37 +0100

    Dodanie możliwości wylogowania użytkownika
    1. Dodanie ścieżki widoku logout w users/urls.py
    
    2. Dodanie widoku logout_view w views.py
    
    3. Dołączenie odnośnika wylogowywania do base.html

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.views import LoginView #importowanie wbudowanego widoku LoginView
 from django.urls import path #importowanie urls path
+from . import views
 
 app_name = 'users' #przypisanie nazwy aplikacji konieczne
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name = 'users/login.html'), name='login'), #utworzenie ścieżki z użyciem klasy LoginView i przypisanie własnego szablonu html
+    path('logout/', views.logout_view, name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect #import redirect
+from django.contrib.auth import logout
 
 # Create your views here.
+
+#Utworzenie funkcji wylogowania
+def logout_view(request):
+    logout(request)
+    return redirect('learning_logs:index')
